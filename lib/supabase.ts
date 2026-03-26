c195d89ec05bae202cb1ad23945537264a388c4c
Ahmed Bayome <115879242+ahmed-bayome@users.noreply.github.com>
2026-03-26 19:39:14 +0200

    fix env keys temp

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
+const NEXT_PUBLIC_SUPABASE_URL = "https://mxjsqsthwteicvuqvisg.supabase.co";
+const SUPABASE_SECRET_KEY = "sb_publishable_L3wEugAFgJEXbkskWlluOw_xO9C3_a0";
 
+
+export const supabase = createClient(
+  NEXT_PUBLIC_SUPABASE_URL,
+  SUPABASE_SECRET_KEY,
+);
